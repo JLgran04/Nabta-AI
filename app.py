62ebed310a69913943d17ed0ac4b90cafab1452f
JLgran04 <153181166+JLgran04@users.noreply.github.com>
2026-03-10 11:11:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -408,9 +408,7 @@ def show_auth_page():
         <div class="auth-wrap">
             <div class="hero">
                 <h1 style="text-align: center;">🌿 Nabta AI</h1>
-                <p> style="text-align: center;">
-                    Working Towards A Greener Kuwait.
-                </p>
+                <p style="text-align: center;"> Working Towards A Greener Kuwait. </p>
             </div>
         </div>
     """, unsafe_allow_html=True)
@@ -638,6 +636,7 @@ elif st.session_state.role == "user":
     show_user_page()
 else:
     st.error("Unknown role detected.")
+
 
 
 
